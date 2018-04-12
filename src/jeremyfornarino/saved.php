09b1a31e2aa13b7b3536f3be0f8526328924fd6e
jeremy-f <jeremy.fornarino@gmail.com>
2018-04-12 03:09:08 +0200

    Update Saved

--- src/jeremyfornarino/saved.php
+++ src/jeremyfornarino/saved.php
@@ -1,8 +1,8 @@
 <?php
 namespace jeremyfornarino;
 
-require_once "../../vendor/autoload.php";
-require_once "config.php";
+require_once __DIR__."/../../vendor/autoload.php";
+require_once __DIR__."/config.php";
 
 use jeremyfornarino\rfCached\Run;
 
@@ -11,5 +11,5 @@ $run = new Run();
 foreach (glob(DIR_RESULTS."saved/*.json") AS $filePath){
     $content = json_decode(file_get_contents($filePath));
     $run->realySave($content->antenna,$content->band, $content->fileName, $content->type);
-    unlink($filePath);
+    //unlink($filePath);
 }
