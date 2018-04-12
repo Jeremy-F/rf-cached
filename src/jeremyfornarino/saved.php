0f0d97ca8b5bd10ecde515be5820123db9b7d1a3
jeremy-f <jeremy.fornarino@gmail.com>
2018-04-12 03:25:10 +0200

    UNlinK  repare

--- src/jeremyfornarino/saved.php
+++ src/jeremyfornarino/saved.php
@@ -11,5 +11,5 @@ $run = new Run();
 foreach (glob(DIR_RESULTS."saved/*.json") AS $filePath){
     $content = json_decode(file_get_contents($filePath));
     $run->realySave($content->antenna,$content->band, $content->fileName, $content->type);
-    //unlink($filePath);
+    unlink($filePath);
 }
