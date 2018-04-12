c59e6a9fa1aaf984ac32e30739c812d5ad245bab
jeremy-f <jeremy.fornarino@gmail.com>
2018-04-12 02:19:57 +0200

    Update config

--- src/jeremyfornarino/config.php
+++ src/jeremyfornarino/config.php
@@ -1,9 +1,7 @@
 <?php
 
-//define("DATA_JSON", "/home/rf/rf-vue/api/data.json");
-define("DATA_JSON", "/Users/jeremyfornarino/Documents/E4_SI/Projet/rf-cached/data.json");
+define("DATA_JSON", "/home/rf/rf-vue/api/data.json");
 define("DIR_ANTENNAS",  "/home/rf/data/");
-//define("DIR_RESULTS",  "/home/rf/rf-vue/api/results/");
-define("DIR_RESULTS",  "/Users/jeremyfornarino/Documents/E4_SI/Projet/rf-cached/");
+define("DIR_RESULTS",  "/home/rf/rf-vue/api/results/");
 
 ?>
